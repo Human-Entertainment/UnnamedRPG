8ee75078795c0cf750e2c50f9e76c3d1b09453e8
Bastian Inuk Christensen <bastian@inuk.blog>
2020-10-15 22:55:38 +0200

    Initial Commit

---
+++ Shared/Unnamed_RPGApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unnamed_RPGApp.swift
+//  Shared
+//
+//  Created by Bastian Inuk Christensen on 15/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct Unnamed_RPGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

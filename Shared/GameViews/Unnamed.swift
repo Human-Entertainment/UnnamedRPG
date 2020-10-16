8f72b52d4e4bf074448764538a889e565fa6c7a6
Bastian Inuk Christensen <bastian@inuk.blog>
2020-10-16 20:35:24 +0200

    Initial game scene

---
+++ Shared/GameViews/Unnamed.swift
@@ -0,0 +1,8 @@
+import ImagineEngine
+@_exported import struct ImagineEngine.Size
+
+class UnnamedScene: Scene  {
+    override func setup() {
+        backgroundColor = Color(red: 0, green: 0, blue: 0.3, alpha: 1)
+    }
+}

987726d2abc3bf11fb2b0531cc3e6d32a3686dc2
Bastian Inuk Christensen <bastian@inuk.blog>
2020-10-16 20:37:12 +0200

    Initial debug for gamepad and imagine engine through SwiftUI

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var gamePad = ControllerPublisher()
+    @State var color = Color.primary
+    
+    let scene = UnnamedScene(size: Size(width: 375, height: 667))
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello World \(color.description)")
+                .padding()
+            if gamePad.mainButtonPressed {
+                Text("Button A pressed")
+            }
+            Test(color: $color)
+            
+        }.background(color)
+        
+    }
+}
+
+struct Test: View {
+    @Binding var color: Color
+    
+    var body: some View {
+        Text("\(color.description)")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
+            
     }
 }
